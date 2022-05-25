89c93f3001ba73790baa1d0c2e8d95092d84ca84
SKero2004 <toyatakahashi522@gmail.com>
2022-05-25 18:25:41 +0900

    patched gaps

--- config.h
+++ config.h
@@ -1,7 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx     = 7;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -59,8 +60,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *upvol[] = { "amixer", "set", "Master", "5%+", NULL };
-static const char *downvol[] = { "amixer", "set", "Master", "5%-", NULL };
+static const char *upvol[] = { "amixer", "set", "Master", "2%+", NULL };
+static const char *downvol[] = { "amixer", "set", "Master", "2%-", NULL };
 static const char *mutevol[] = { "amixer", "set", "Master", "toggle", NULL };
 static const char *upbr[] = { "xbacklight", "-inc", "5" ,NULL };
 static const char *downbr[] = { "xbacklight", "-dec", "5" ,NULL };
@@ -95,6 +96,9 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_0,      					tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  					focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, 					focusmon,       {.i = +1 } },
+	{ MODKEY,                       XK_minus,  					setgaps,        {.i = -1 } },
+	{ MODKEY,                       XK_equal,  					setgaps,        {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_equal,  					setgaps,        {.i = 0  } },
 	{ MODKEY|ShiftMask,             XK_comma,  					tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, 					tagmon,         {.i = +1 } },
 	{ MODKEY|ShiftMask,				XK_l,	   					shiftview,	    {.i = +1 } },
