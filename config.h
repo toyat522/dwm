229bc05140d095d459974959ee5792f825dab629
SKero2004 <toyatakahashi522@gmail.com>
2022-05-26 17:31:18 +0900

    Added active tag indicator bar

--- config.h
+++ config.h
@@ -1,11 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 8;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 28;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "monospace:size=10", "Font Awesome 6 Brands Regular:size=11", "Font Awesome 6 Free Solid:size=11", "Font Awesome v4 Compatibility Regular:size=11", "Font Awesome 6 Free Regular:size=11" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
